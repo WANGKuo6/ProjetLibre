6302b0f280ae9fd52c566678f1b21d390dbe036e
wangkuo <291974520@qq.com>
2021-02-16 10:16:13 +0100

    add cookie

--- mysql/views.py
+++ mysql/views.py
@@ -10,11 +10,18 @@ def login(request):
         res_user = User.objects.get(user_name=username)
         if password == res_user.password:
             print("456")
-            return redirect('/index/')
+            rep = redirect('/index/')
+            rep.set_cookie("is_login", True)
+            rep.set_cookie("username", username)
+            return rep
     return render(request, 'login.html')
 
 def index(request):
     return render(request, 'Login.html')
 
 def Accueil(request):
+    status = request.COOKIES.get("is_login")
+    print(status)
+    if not status:
+        return redirect('/login/')
     return render(request, 'index.html')
